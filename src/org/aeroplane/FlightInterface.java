17db3c444ced897f321da70f1ff7b939a2502aa5
rhythm1234567 <47144213+rhythm1234567@users.noreply.github.com>
2019-04-17 17:27:17 +0800

    Delete FlightInterface.java

--- src/org/aeroplane/FlightInterface.java
+++
@@ -1,9 +0,0 @@
-
-package org.aeroplane;
-
-
-public interface FlightInterface {
-public int[] reserve(String[] names);
-public boolean cancel(int bookingNumber);
-public Passenger[] getPassengerList();
-}
